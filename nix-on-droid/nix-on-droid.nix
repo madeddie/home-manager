d82a3e92869df032de8918dae5bc622910e35904
Edwin Hermans <edwin@madtech.cx>
2025-05-01 17:53:50 -0400

    Add nix-on-droid config

---
+++ nix-on-droid/nix-on-droid.nix
@@ -0,0 +1,53 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    #git
+    openssh
+    wget
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    #man
+    gawk
+    gnugrep
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  #environment.pathsToLink = [ "/share/bash-completion" ];
+
+  environment.etc."resolv.conf".text = lib.mkForce ''
+    nameserver 192.168.0.1
+  '';
+
+  # Read the changelog before changing this value
+  system.stateVersion = "24.05";
+
+  # Set up nix for flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  # Set your time zone
+  time.timeZone = "America/New_York";
+
+  user.shell = "${pkgs.zsh}/bin/zsh";
+
+  android-integration.termux-setup-storage.enable= true;
+}
+
+# vim: ft=nix
