0392d33abaac8eeaf5b9217fcb34f140100b8520
Edwin Hermans <edwin@madtech.cx>
2025-11-28 10:36:19 -0500

    Update to newer home manager, fix sops secret aws config

--- home_nix-on-droid.nix
+++ home_nix-on-droid.nix
@@ -5,7 +5,7 @@
 }: {
   home.username = "nix-on-droid";
   home.homeDirectory = "/data/data/com.termux.nix/files/home";
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.11";
 
   home.sessionVariables = {
     TODO_DIR = "$HOME/storage/documents/markor";
