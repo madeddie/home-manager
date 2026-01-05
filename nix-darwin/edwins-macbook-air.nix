a8f4006748665e07396148822b55d2608af0e8ae
Edwin Hermans <edwin@madtech.cx>
2026-01-05 13:04:28 -0500

    Add OBS and 1password-cli, update flakes

--- nix-darwin/edwins-macbook-air.nix
+++ nix-darwin/edwins-macbook-air.nix
@@ -7,10 +7,13 @@
     ];
     casks = [
       "1password"
+      "1password-cli"
+      "droidcam-obs"
       "google-chrome"
       "minecraft"
       "moonlight"
       "nvidia-geforce-now"
+      "obs"
       "slack"
       "tailscale-app"
       "zoom"
