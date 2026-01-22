bc209a5c4ce56b05ea564a32b6d4e90b3be15fa1
Edwin Hermans <edwin@madtech.cx>
2026-01-22 12:45:38 -0500

    Add additional porter funcs

--- dotfiles/porter/aliases.zsh
+++ dotfiles/porter/aliases.zsh
@@ -18,12 +18,10 @@ awsexport () {
     echo "Missing project id"
     return
   fi
-  local cmd="ccp-cli manage cloud aws --project-id $1"
   if [[ -n "$2" ]]; then
-    cmd+=" --aws-account-id $2"
+    local aws_account="--aws-account-id $2"
   fi
-  cmd+=" -- configure export-credentials --format=env"
-  eval $($=cmd)
+eval $(ccp-cli manage cloud aws --project-id $1 ${(z)aws_account} -- configure export-credentials --format=env)
 }
 
 function pllc {
@@ -56,6 +54,23 @@ function plookup {
   curl -s -H "Authorization: Bearer $(op item get api.usepylon.com --fields credential --reveal)" $api_url -d "$lookup_json" | jq -r '.data[] | "\(.name): \(.custom_fields.account_project_id.value)"'
 }
 
+function pk9s {
+  local KUBECONFIG=$(mktemp)
+  trap "rm -f $KUBECONFIG" EXIT
+  if [[ -n $1 ]]; then
+    local project="--project $1"
+  fi
+  if [[ -n $2 ]]; then
+    local cluster="--cluster $2"
+  fi
+  porter ${(z)project} ${(z)cluster} kubectl --print-kubeconfig >! $KUBECONFIG && KUBECONFIG=$KUBECONFIG k9s
+}
+
+function pset {
+  porter config set-project $1
+  porter config set-cluster $2
+}
+
 alias k=kubectl
 alias pk="porter kubectl --"
 alias ph="porter helm --"
@@ -76,7 +91,7 @@ alias pin-there="pin --host=https://dashboard.porter.run"
 alias backup-fig="cp -i ~/.aws/config_backup ~/.aws/config"
 alias gpo="git pull origin"
 alias gba='for k in `git branch|sed s/^..//`;do echo -e `git log -1 --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" "$k"`\\t"$k";done|sort'
-alias pk9s="porter kubectl --print-kubeconfig >! /tmp/temp-kubeconfig && KUBECONFIG=/tmp/temp-kubeconfig k9s"
+#alias pk9s="porter kubectl --print-kubeconfig >! /tmp/temp-kubeconfig && KUBECONFIG=/tmp/temp-kubeconfig k9s"
 alias pkc="porter kubectl --print-kubeconfig >! /tmp/temp-kubeconfig && KUBECONFIG=/tmp/temp-kubeconfig"
 
 alias gm="goose -dir zarf/database/migrations"
